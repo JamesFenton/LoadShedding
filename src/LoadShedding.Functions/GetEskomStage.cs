38e1b9add021d3bf937cbd7cce96f49d5ebce467
James Fenton <jamesfenton777@gmail.com>
2019-03-20 22:18:05 +0200

    Get and store current stage every 5 minutes and save history once per hour

---
+++ src/LoadShedding.Functions/GetEskomStage.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading.Tasks;
+using LoadShedding.Functions.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace LoadShedding.Functions
+{
+    public static class GetEskomStage
+    {
+        private static readonly EskomService _eskomService = new EskomService();
+
+        [FunctionName("GetEskomStage")]
+        public static async Task Run(
+            [TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, // once every five minutes
+            [Blob("stage-data/current-stage.txt")] CloudBlockBlob currentEskomStage, // update current stage
+            [Blob("notifications/stage-changed-people-to-notify.txt")] string peopleToNotify, // new-line separated list of phone numbers
+            [TwilioSms(AccountSidSetting = "TwilioAccountSid", AuthTokenSetting = "TwilioAuthToken", From = "+13476090886")] ICollector<CreateMessageOptions> messagesToSend,
+            ILogger log)
+        {
+            // get previous stage
+            log.LogInformation($"Getting previous stage");
+            var previousStage = int.Parse(await currentEskomStage.DownloadTextAsync());
+
+            // get current stage
+            log.LogInformation($"Getting current stage");
+            var currentStage = await _eskomService.GetEskomStage();
+
+            // notify if stage changed
+            if (previousStage != currentStage)
+            {
+                log.LogInformation($"Stage changed from {previousStage} to {currentStage}");
+
+                // send sms's
+                var numbers = peopleToNotify.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                log.LogInformation($"Notifying {numbers.Length} people");
+                foreach (var number in numbers)
+                {
+                    messagesToSend.Add(new CreateMessageOptions(new PhoneNumber(number))
+                    {
+                        Body = $"Loadshedding is now stage {currentStage} (was {previousStage})",
+                    });
+                }
+            }
+            else
+            {
+                log.LogInformation($"Stage unchanged at {currentStage}");
+            }
+        }
+    }
+}
---
+++ src/LoadShedding.Functions/SaveStageHistory.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using LoadShedding.Functions.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace LoadShedding.Functions
+{
+    public static class SaveStageHistory
+    {
+        [FunctionName("SaveStageHistory")]
+        public static async Task Run(
+            [TimerTrigger("0 0 * * * *")] TimerInfo myTimer, // once per hour
+            [Blob("stage-data/current-stage.txt")] CloudBlockBlob currentEskomStage, // get current stage
+            [Table("EskomStageHistory")] CloudTable eskomStageHistoryTable, // save entry in history
+            ILogger log)
+        {
+            // get current stage
+            log.LogInformation($"Getting current stage");
+            var currentStage = int.Parse(await currentEskomStage.DownloadTextAsync());
+            
+            // save to history
+            log.LogInformation($"Saving eskom stage {currentStage}");
+            var entity = new EskomStage(now: DateTimeOffset.UtcNow, stage: currentStage);
+            var operation = TableOperation.InsertOrReplace(entity);
+            await eskomStageHistoryTable.ExecuteAsync(operation);
+        }
+    }
+}
